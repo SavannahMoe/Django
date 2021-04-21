94cf0b5e2865cf10547578a7e769bc2da642738c
Savannah Moe <savannah_moe@baylor.edu>
2021-04-21 15:43:44 -0500

    creating forms to add new topics and new entries

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -20,3 +21,33 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'MainApp/topic.html', context)
 
+def new_topic(request):
+    if request.method != 'POST': #not post which means get 
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save() #know to relate to topic model be
+
+            return redirect('MainApp:topics')
+
+    context = {'form': form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False) #not writing to database because topic is not assigned yet
+            new_entry.topic = topic
+            new_entry.save()
+            
+            return redirect('MainApp:topic', topic_id=topic.id)
+
+    context = {'form': form, 'topic':topic}
+    return render(request, 'MainApp/new_entry.html', context)
